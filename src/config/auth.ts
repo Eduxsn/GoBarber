fba5f55161c935bd4e5eae34560ab8d3ba988e3f
Eduardo <eduneves95@gmail.com>
2020-07-04 21:39:26 -0300

    Add routes to validate user authentication

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+    jwt: {
+        secret: '40d50cbf62a508b6078ac5b3cfa07e66',
+        expiresIn: '1d',
+    },
+};
--- src/middleares/ensureAuthenticated.ts
+++ src/middleares/ensureAuthenticated.ts
@@ -1,3 +1,37 @@
-import { request } from 'express';
+import { Request, Response, NextFunction } from 'express';
+import { verify } from 'jsonwebtoken';
+import authConfig from '../config/auth';
 
-export default function ensudeAuthenticarted(request, response, next): void {}
+interface TokenPayload {
+    iat: number;
+    exp: number;
+    sub: string;
+}
+
+export default function ensudeAuthenticarted(
+    request: Request,
+    response: Response,
+    next: NextFunction,
+): void {
+    const authHeader = request.headers.authorization;
+
+    if (!authHeader) {
+        throw new Error('JWT token is missing');
+    }
+
+    const [, token] = authHeader.split(' ');
+
+    try {
+        const decoded = verify(token, authConfig.jwt.secret);
+
+        const { sub } = decoded as TokenPayload;
+
+        request.user = {
+            id: sub,
+        };
+
+        return next();
+    } catch {
+        throw new Error('Invalid JWT token');
+    }
+}
